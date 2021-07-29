acf2c587b032fe558ecc0e3a4a8d92b65dcfa0ca
Nikhil Dhawan <nikhild64@gmail.com>
2021-07-29 16:35:24 +0530

    added trackby for ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+interface Item {
+  id: number;
+  name: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'trackby-example';
+  items: Item[] = [
+    { id: 1, name: 'HTML' },
+    { id: 2, name: 'CSS' },
+    { id: 3, name: 'JavaScript' },
+  ];
+  addItem() {
+    this.items = [
+      { id: 1, name: 'HTML' },
+      { id: 2, name: 'CSS' },
+      { id: 3, name: 'JavaScript' },
+      {
+        id: 4,
+        name: 'Angular',
+      },
+    ];
+  }
+  trackBy(index: number, item: Item) {
+    return item.id;
+  }
 }
